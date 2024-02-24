92149a6a29e4143f552b4fb968d47c57497b637d
Faith-Nyambura <faithnyamburawamui@gmail.com>
2024-02-24 21:37:41 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+fun main(){
+    writeFullName()
+    writeAge()
+    writeFullName()
+    writeWeightIn()
+    writeCitizen()
+
+}
+
+fun writeFullName(){
+    var firstname ="Faith"
+    var lastname ="Nyambura"
+    var result="Faith" + "Nyambura"
+    println(result)
+}
+
+fun writeAge (){
+    var num1 =45
+    var result="45"
+    println(result)
+}
+
+fun writePhoneNumber(){
+    var phoneNumber=String
+    var results="string"
+    println(results)
+
+}
+
+fun writeWeightIn(){
+    var weightInKg=50
+    var result="50"
+    println(result)
+}
+
+fun writeCitizen(){
+    var citizen="Kenyan"
+    var result="Kenyan"
+    println(result)
+
+}
